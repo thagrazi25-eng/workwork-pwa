e627c2e0f148c6f99d3440bba999f06f0e470701
thagrazi25-eng <thagrazi25-eng@users.noreply.github.com>
2026-05-01 14:27:33 -0300

    chore: setup github pages base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/workwork-pwa/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
